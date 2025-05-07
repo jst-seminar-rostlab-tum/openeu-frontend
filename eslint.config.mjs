223643c9fb069acc8e09b16c8735fec9d30b20d0
Trung Nguyen <trungnguyenb0k30@gmail.com>
2025-05-07 02:55:57 +0200

    adding lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    ignores: [
+      '.now/*',
+      '*.css',
+      '.changeset',
+      'dist',
+      'esm/*',
+      'public/*',
+      'tests/*',
+      'scripts/*',
+      '*.config.js',
+      '.DS_Store',
+      'node_modules',
+      'coverage',
+      '.next',
+      'build',
+
+      // EXCEPTIONS (negated ignores not supported in ESLint 9+ directly)
+      // You must explicitly include those files if needed in tooling/tests
+    ],
+  },
 ];
 
 export default eslintConfig;
