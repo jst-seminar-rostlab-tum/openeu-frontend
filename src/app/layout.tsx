e35950a7805b2ba482261e068bf2154f8bb3475e
Trung Nguyen <trungnguyenb0k30@gmail.com>
2025-05-15 00:07:02 +0200

    feat: add theme-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import '../styles/globals.css';
-import type { Metadata } from 'next';
-import { cn } from '@/lib/utils';
-import { ThemeProvider } from '@/components/ThemeProvider';
 
+import type { Metadata } from 'next';
 import React from 'react';
+
+import { ThemeProvider } from '@/components/ThemeProvider';
+import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
