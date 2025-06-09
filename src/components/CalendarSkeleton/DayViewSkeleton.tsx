444f952f70e9b8fe0a059dec858680989ce576ed
Max-vS <max@vonstorch.com>
2025-06-09 02:54:05 +0200

    chore: improve daily skeleton

--- src/components/CalendarSkeleton/DayViewSkeleton.tsx
+++ src/components/CalendarSkeleton/DayViewSkeleton.tsx
@@ -1,31 +1,63 @@
 import { Skeleton } from '@/components/ui/skeleton';
 
 export function DayViewSkeleton() {
+  const hours = Array.from({ length: 24 }, (_, i) => i);
+
   return (
-    <div className="flex h-full flex-col">
-      <div className="grid grid-cols-2 border-b">
-        <div className="w-18"></div>
-        <div className="flex flex-col items-center justify-center py-2">
-          <Skeleton className="h-6 w-24 rounded-full" />
-          <Skeleton className="mt-1 h-4 w-16" />
+    <div className="flex">
+      <div className="flex flex-1 flex-col">
+        {/* Day header skeleton */}
+        <div className="relative z-20 flex border-b">
+          <div className="w-18"></div>
+          <div className="flex-1 border-l py-2 text-center">
+            <Skeleton className="mx-auto h-5 w-20" />
+          </div>
+        </div>
+
+        {/* Calendar grid skeleton */}
+        <div className="flex h-[800px]">
+          {/* Hours column */}
+          <div className="relative w-18">
+            {hours.map((hour) => (
+              <div key={hour} className="relative" style={{ height: '96px' }}>
+                <div className="absolute -top-3 right-2 flex h-6 items-center">
+                  {hour !== 0 && <Skeleton className="h-4 w-12" />}
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Day grid */}
+          <div className="relative flex-1 border-l">
+            <div className="relative">
+              {hours.map((hour) => (
+                <div key={hour} className="relative" style={{ height: '96px' }}>
+                  <div className="pointer-events-none absolute inset-x-0 top-0 border-b"></div>
+                  <div className="pointer-events-none absolute inset-x-0 top-1/2 border-b border-dashed border-b-tertiary"></div>
+
+                  {/* Random event skeletons */}
+                  {hour % 3 === 0 && (
+                    <div className="absolute inset-x-2 top-2 h-20">
+                      <Skeleton className="h-full w-full rounded-md" />
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="flex flex-1 overflow-y-auto">
-        <div className="w-18 flex-shrink-0">
-          {Array.from({ length: 12 }).map((_, i) => (
-            <div key={i} className="relative h-12 border-b pr-2 text-right">
-              <Skeleton className="absolute -top-3 right-2 h-4 w-10" />
-            </div>
-          ))}
+      {/* Simplified right sidebar skeleton */}
+      <div className="hidden w-72 divide-y border-l md:block">
+        {/* Date picker skeleton - single box */}
+        <div className="p-4">
+          <Skeleton className="h-[300px] w-full rounded-lg" />
         </div>
 
-        <div className="flex-1">
-          <div className="relative">
-            {Array.from({ length: 12 }).map((_, hourIndex) => (
-              <div key={hourIndex} className="h-12 border-b"></div>
-            ))}
-          </div>
+        {/* Current events skeleton - single box */}
+        <div className="p-4">
+          <Skeleton className="h-[50px] w-full rounded-lg" />
         </div>
       </div>
     </div>
