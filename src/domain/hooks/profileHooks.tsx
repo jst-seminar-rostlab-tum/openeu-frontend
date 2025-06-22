c4cb5c187e14dd063b43637e24793e63fa69e8bf
ge35tuh <pascal.moser@tum.de>
2025-06-22 12:19:41 +0200

    feat: add missing hook

---
+++ src/domain/hooks/profileHooks.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { useContext } from 'react';
+
+import {
+  IProfileContext,
+  ProfileContext,
+} from '@/components/profile/ProfileContext';
+import { ProfileData } from '@/domain/entities/profile/generated-types';
+import { profileRepository } from '@/repositories/profileRepository';
+
+export const useProfile = (userId?: string) =>
+  useQuery<ProfileData | false>({
+    queryKey: ['profile', userId],
+    queryFn: () => profileRepository.getProfile(userId),
+  });
+
+export function useProfileContext(): IProfileContext {
+  const context = useContext(ProfileContext);
+  if (context === undefined)
+    throw new Error('useProfile must be used within a ProfileProvider.');
+  return context;
+}
