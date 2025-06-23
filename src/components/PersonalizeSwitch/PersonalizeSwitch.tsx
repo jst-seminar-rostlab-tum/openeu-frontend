2b1c1281dfd6ebae8e8cdf2589171d40a8c9063a
Daniel Bier <daniel.bier@tum.de>
2025-06-23 16:26:05 +0200

    feat: disable PersonalizeSwitch.tsx when missing info

--- src/components/PersonalizeSwitch/PersonalizeSwitch.tsx
+++ src/components/PersonalizeSwitch/PersonalizeSwitch.tsx
@@ -7,18 +7,37 @@ import { useAuth } from '@/domain/hooks/useAuth';
 
 export default function PersonalizeSwitch() {
   const { setFilters, filters } = useMeetingContext();
-
   const { user } = useAuth();
+
+  const hasProfile = user?.user_metadata['incompleteProfile'];
+
+  const [checked, setChecked] = React.useState(false);
+
+  React.useEffect(() => {
+    if (hasProfile) {
+      setFilters({
+        ...filters,
+        user_id: user?.id,
+      });
+      setChecked(true);
+    }
+  }, [filters, hasProfile, setFilters, user?.id]);
+
   const handleSwitch = (checked: boolean) => {
+    setChecked(checked);
     setFilters({
       ...filters,
-      user_id: checked && user ? user.id : '',
+      user_id: checked && hasProfile ? user?.id : '',
     });
   };
 
   return (
     <div className="flex items-center space-x-2">
-      <Switch defaultChecked={false} onCheckedChange={handleSwitch} />
+      <Switch
+        checked={checked}
+        onCheckedChange={handleSwitch}
+        disabled={!hasProfile}
+      />
       <Label>Personalized</Label>
     </div>
   );
