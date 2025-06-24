6b42fe002541f70d8998a64c5eab3e8b6288afd9
Tabea Klein <tabea.klein@redpoint.teseon.com>
2025-06-24 10:53:00 +0200

    feat: add avatar stack

---
+++ src/domain/entities/mock/mock_members.ts
@@ -0,0 +1,58 @@
+import { Member } from '../calendar/generated-types';
+
+export const members: Member[] = [
+  {
+    id: '1',
+    type: 'person',
+    label: 'Carla Newton',
+    family_name: 'Newton',
+    given_name: 'Carla',
+    sort_label: 'Newton, Carla',
+    country: 'Poland',
+  },
+  {
+    id: '2',
+    type: 'person',
+    label: 'Lara Roft',
+    family_name: 'Roft',
+    given_name: 'Lara',
+    sort_label: 'Roft, Lara',
+    country: 'Poland',
+  },
+  {
+    id: '3',
+    type: 'person',
+    label: 'Erik Doe',
+    family_name: 'Doe',
+    given_name: 'Erik',
+    sort_label: 'Doe, Erik',
+    country: 'Poland',
+  },
+  {
+    id: '4',
+    type: 'person',
+    label: 'Sarah Crow',
+    family_name: 'Crow',
+    given_name: 'Sarah',
+    sort_label: 'Crow, Sarah',
+    country: 'Poland',
+  },
+  {
+    id: '5',
+    type: 'person',
+    label: 'Hank Barnes',
+    family_name: 'Barnes',
+    given_name: 'Hank',
+    sort_label: 'Barnes, Hank',
+    country: 'Poland',
+  },
+  {
+    id: '6',
+    type: 'person',
+    label: 'June Park',
+    family_name: 'Park',
+    given_name: 'June',
+    sort_label: 'Park, June',
+    country: 'Poland',
+  },
+];
