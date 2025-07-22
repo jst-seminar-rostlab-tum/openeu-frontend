515c8d983af3c7cc3f250aa13c86529e8f11d827
Magnus Singer <work@magnus-singer.com>
2025-07-22 11:58:59 +0200

    feat: show if event is already in calendar

--- src/domain/actions/google-calendar.ts
+++ src/domain/actions/google-calendar.ts
@@ -62,3 +62,29 @@ export async function saveToCalendar(
 
   return false;
 }
+
+export async function getEvents(startDate: Date, endDate: Date) {
+  const supabase = await createClient();
+  const userData = await supabase.auth.getUser();
+  if (!userData.data.user) {
+    throw new Error('No user data provided');
+  }
+  const userMetadata = userData.data.user.user_metadata;
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI,
+  );
+  oauth2Client.credentials.refresh_token = userMetadata['oauthRefreshToken'];
+  await oauth2Client.refreshAccessToken();
+
+  const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+  const events = await calendar.events.list({
+    calendarId: 'primary',
+    timeMin: startDate.toISOString(),
+    timeMax: endDate.toISOString(),
+  });
+  if (events.data.items !== undefined && events.data.items.length > 0) {
+    return events.data.items;
+  }
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -230,3 +230,11 @@ export function getWeekKey(date: Date): string {
   );
   return `${d.getUTCFullYear()}-W${weekNo}`;
 }
+
+export function b64EncodeUnicode(str: string) {
+  return btoa(
+    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+      return String.fromCharCode(Number('0x' + p1));
+    }),
+  );
+}
